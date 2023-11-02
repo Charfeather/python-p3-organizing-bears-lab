0ab8bb5c72320da946fef8cef8a21b67c5ea5b19
charlesfeatherstone <charlesfstone11@gmail.com>
2023-11-02 11:40:55 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('MR.Chocolate',4,'M','brown','nice',TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Rowdy',4,'M','polar','rowdy',TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Tabitha',4,'F','black','nice',TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Sergeamt Brown',4,'M','brown','nice',TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Melissa',4,'F','brown','nice',TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Grinch',4,'M','brown','rude',TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Wendy',4,'f','brown','nice',TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL,4,'M','brown','nice',TRUE);
